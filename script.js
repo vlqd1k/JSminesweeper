43e34c3531ae2f6e3a7725238dafc12537f8c9fe
vlqd1k <125270299+vlqd1k@users.noreply.github.com>
2023-03-20 16:02:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ import {
 } from "./minesweeper.js"
 
 const BOARD_SIZE = 10
-const NUMBER_OF_MINES = 5
+const NUMBER_OF_MINES = 10
+
 
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
 const boardElement = document.querySelector(".board")
