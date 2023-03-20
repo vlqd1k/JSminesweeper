b91111f992535cfcc88f0062a2228fec25b31534
vlqd1k <125270299+vlqd1k@users.noreply.github.com>
2023-03-20 16:01:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ import {
 } from "./minesweeper.js"
 
 const BOARD_SIZE = 10
-const NUMBER_OF_MINES = 3
+const NUMBER_OF_MINES = 5
 
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
 const boardElement = document.querySelector(".board")
